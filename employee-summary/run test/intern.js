8a65c7256e53fb19b38432e44e9a98ee7a27b0c0
bmartinez2018 <67445858+bmartinez2018@users.noreply.github.com>
2020-09-12 11:37:04 -0500

    Add files via upload

---
+++ employee-summary/run test/intern.js
@@ -0,0 +1,17 @@
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+	constructor(name, id, email, school) {
+		super(name, id, email);
+		this.school = school;
+		this.role = "Intern";
+	}
+	getRole() {
+		return "Intern";
+	}
+	getSchool() {
+		return this.school;
+	}
+}
+
+export default Intern;
